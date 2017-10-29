49ead7818ea47eb4464d716d27d630c4f43907af
Jack Leshem <jsleshem@wustl.edu>
2017-10-28 20:10:50 -0500

    ahhh

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -12,6 +12,7 @@ var currentUser = null;
 
 
 function showPage(id) {
+	//document.getElementsByClassName("inputForm").value = "";
 	console.log(id + " is the page loading");
 	if (id == "login") {
 		currentUser = null;
@@ -26,14 +27,16 @@ function showPage(id) {
 }
 
 document.getElementById("loginPageBTN").addEventListener("click", function () {
-		if (document.getElementById("userBarCode").value === barCode || document.getElementById("userBarName").value === barName){
-			showPage("login");
-			document.getElementById("currentBarName").innerHTML = barName;
-		}
-		else{
-			showPage("barDNE");
-		}
+	if (document.getElementById("userBarCode").value === barCode || document.getElementById("userBarName").value === barName){
+		showPage("login");
+		document.getElementById("currentBarName").innerHTML = barName;
 	}
+	else{
+		showPage("barDNE");
+	}
+	document.getElementById("userBarCode").value = "";
+	document.getElementById("userBarName").value = "";
+}
 )
 
 document.getElementById("loginBTN").addEventListener("click", function () {
@@ -46,10 +49,16 @@ document.getElementById("loginBTN").addEventListener("click", function () {
 			exists = true;
 			if(userAll[i] === currentUser){
 				showPage("homePage");
+				document.getElementById("username").value = "";
+				document.getElementById("userPin").value = "";
+				document.getElementById("userNumber").value = "";
 				document.getElementById("userHomeName").innerHTML = currentName;
 			}
 			else{
 				showPage("incorrectPass");
+				document.getElementById("username").value = "";
+				document.getElementById("userPin").value = "";
+				document.getElementById("userNumber").value = "";
 			}
 		}
 	}
@@ -57,36 +66,42 @@ document.getElementById("loginBTN").addEventListener("click", function () {
 		userNames.push(currentName);
 		userNumbers.push(currentNumber);
 		userAll.push(currentUser);
-	  showPage("homePage");
-	  document.getElementById("userHomeName").innerHTML = currentName + "<br><h3>Thanks for signing up!</h3>";
+		showPage("homePage");
+		document.getElementById("userHomeName").innerHTML = currentName + "<br><h3>Thanks for signing up!</h3>";
+		document.getElementById("username").value = "";
+		document.getElementById("userPin").value = "";
+		document.getElementById("userNumber").value = "";
 	}
+	document.getElementById("username").value = "";
+	document.getElementById("userPin").value = "";
+	document.getElementById("userNumber").value = "";
 }
 )
 
 document.getElementById("logout").addEventListener("click", function () {
-		showPage("login");
-	}
+	showPage("login");
+}
 )
 
 document.getElementById("logout2").addEventListener("click", function () {
-		showPage("login");
-	}
+	showPage("login");
+}
 )
 
 document.getElementById("requestConfirm").addEventListener("click", function () {
-		showPage("requestConfirmPage");
-	}
+	showPage("requestConfirmPage");
+}
 )
 
 document.getElementById("change1").addEventListener("click", function () {
-	  currentUser = null;
-		showPage("landing");
-	}
+	currentUser = null;
+	showPage("landing");
+}
 )
 
 document.getElementById("headHome").addEventListener("click", function () {
-		showPage("landing");
-	}
+	showPage("landing");
+}
 )
 
 document.getElementById("reqBTN2").addEventListener("click", function () {
@@ -102,9 +117,9 @@ document.getElementById("reqBTN2").addEventListener("click", function () {
 		}
 	}
 	else{
-		 showPage("noSpace");
-	 }
+		showPage("noSpace");
 	}
+}
 )
 
 document.getElementById("reqBTN").addEventListener("click", function () {
@@ -116,72 +131,84 @@ document.getElementById("reqBTN").addEventListener("click", function () {
 				document.getElementById("reqName").innerHTML = currentName;
 				lockers[i] = currentUser;
 				break;
-	    }
-	  }
+			}
+		}
 	}
-  else{
-	   showPage("noSpace");
-   }
- }
+	else{
+		showPage("noSpace");
+	}
+}
 )
 
 document.getElementById("retBTN").addEventListener("click", function () {
-		hasLocker = false;
-		lockerNumber = null;
-		for (i = 0; i < lockers.length; i++){
-			if (lockers[i] === currentUser){
-				hasLocker = true;
-				lockerNumber = i;
-			}
+	hasLocker = false;
+	//canRetrieve
+	lockerNumber = null;
+	for (i = 0; i < lockers.length; i++){
+		if (lockers[i] === currentUser){
+			hasLocker = true;
+			lockerNumber = i;
 		}
-		if(hasLocker)	{
-			lockers[lockerNumber] = null;
-			//Open the door
+	}
+	if(hasLocker)	{
+		lockers[lockerNumber] = null;
+		//See status from sensor
+		//-1 --> drunk and no keys
+		//0 --> not drunk, get keys
+		getTheKeys = -1;
+		if (getTheKeys === 0){
 			document.getElementById("openingLocker").innerHTML = " " + (lockerNumber + 1);
+			document.getElementById("canRetrieve").innerHTML = "Ready to open, have a safe night!"
 			showPage("keyReturned");
 		}
-		else {
-			showPage("noLocker");
+		else{
+			document.getElementById("openingLocker").innerHTML = " " + (lockerNumber + 1);
+			document.getElementById("canRetrieve").innerHTML = "Sorry, but you are above the legal limit."
+			showPage("keyReturned");
 		}
-
 	}
+else {
+	showPage("noLocker");
+}
+
+}
 )
 
 
 document.getElementById("change2").addEventListener("click", function () {
-	  currentUser = null;
-		showPage("landing");
-	}
+	currentUser = null;
+	showPage("landing");
+}
 )
 
 document.getElementById("change3").addEventListener("click", function () {
-	  currentUser = null;
-		showPage("landing");
-	}
+	currentUser = null;
+	showPage("landing");
+}
 )
 
 document.getElementById("change4").addEventListener("click", function () {
-	  currentUser = null;
-		showPage("landing");
-	}
+	currentUser = null;
+	showPage("landing");
+}
 )
 
 document.getElementById("change5").addEventListener("click", function () {
-	  currentUser = null;
-		showPage("landing");
-	}
+	currentUser = null;
+	showPage("landing");
+}
 )
 
 document.getElementById("change6").addEventListener("click", function () {
-	  currentUser = null;
-		showPage("landing");
-	}
+	currentUser = null;
+	showPage("landing");
+}
 )
 
 document.getElementById("change7").addEventListener("click", function () {
-	  currentUser = null;
-		showPage("landing");
-	}
+	currentUser = null;
+	showPage("login");
+}
 )
 
 showPage("landing");
