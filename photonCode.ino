3341cefafdaa5810cdc8022c257d8a8dc8f2631c
dpollak4 <dpollak@wustl.edu>
2017-10-28 23:46:58 -0500

    Added LED to see if box available

--- photonCode.ino
+++ photonCode.ino
@@ -12,6 +12,14 @@ Servo servoList[2];
 #define servoOutPin0 D0
 #define servoOutPin1 D1
 
+//LED Pins, list format: led0Green, led1Green, etc
+#define LED_LIST_LENGTH 2
+int ledListGreen[LED_LIST_LENGTH] = {A2, A4};
+int ledListRed[LED_LIST_LENGTH] = {A3, A5};
+#define ledOn LOW
+#define ledOff HIGH
+
+
 //Ethanol Sensor Pins
 int gasDigitalIn = 3;
 int gasAnalogIn = A0;
@@ -21,10 +29,10 @@ int analogReadValue;
 bool openBox = false;
 
 //Constants
-#define SENSOR_MAX_IDLE 700
-#define SENSOR_MAX_OPEN 1000
-#define SERVO_OPEN 10
-#define SERVO_CLOSED 170
+#define SENSOR_MAX_IDLE 800
+#define SENSOR_MAX_OPEN 1300
+#define SERVO_OPEN 200
+#define SERVO_CLOSED 130
 #define NUM_MEASUREMENTS 12
 #define MAX_MEASUREMENT_WAIT 12
 #define HALF_SECOND 500
@@ -35,20 +43,29 @@ void setup()
     pinMode(gasDigitalIn, INPUT);
     pinMode(gasAnalogIn, INPUT);
 
-    //Initialize servos to "open"
-    
+    //Initialize servos to "closed"
     servoList[0].attach(servoOutPin0);
-    servoList[0].write(SERVO_OPEN);
+    servoList[0].write(SERVO_CLOSED);
     servoList[1].attach(servoOutPin1);
-    servoList[1].write(SERVO_OPEN);
-  
+    servoList[1].write(SERVO_CLOSED);
+    
+    //Initialize LED pins to available green
+    for(int i = 0; i < LED_LIST_LENGTH; i++)
+    {
+        pinMode(ledListGreen[i], OUTPUT);
+        pinMode(ledListRed[i], OUTPUT);
+        digitalWrite(ledListGreen[i], ledOn);
+        digitalWrite(ledListRed[i], ledOff);
+    }
+    
     //Sets up the Neopixel ring to start off
     strip.begin();
     strip.show();
     
     //Functions to be called by the webpage
-    Particle.function("closeDoor",closeDoor);
-    Particle.function("runSensor",runSensor);
+    Particle.function("openDoor", openDoor);
+    Particle.function("closeDoor", closeDoor);
+    Particle.function("runSensor", runSensor);
 
     Serial.begin(9600);
 }
@@ -58,12 +75,27 @@ void loop()
 
 }
 
-//Called by the webpage to lock a door with new key in it
+//Called by the webpage to open a locked, available door to put key in
+int openDoor(String servoIndexString)
+{
+    int servoIndex = atoi(servoIndexString);
+    servoList[servoIndex].write(SERVO_OPEN);
+    
+    return 0;
+}
+
+//Called by the webpage to lock a door with new key in it, set door LED red
 int closeDoor(String servoIndexString)
 {
     int servoIndex = atoi(servoIndexString);
+    Serial.print("servoIndex ");
+    Serial.println(servoIndex);
     servoList[servoIndex].write(SERVO_CLOSED);
-    return 0;
+    
+    digitalWrite(ledListGreen[servoIndex], ledOff);
+    digitalWrite(ledListRed[servoIndex], ledOn);
+    
+    return -1;
 }
 
 //Called by the webpage to test if door can be opened
@@ -147,11 +179,15 @@ void toggleServo(String servoIndexString)
     {
         Serial.println("OPEN");
         servoList[servoIndex].write(SERVO_OPEN);
+        digitalWrite(ledListGreen[servoIndex], ledOn);
+        digitalWrite(ledListRed[servoIndex], ledOff);
     }
     else if (openBox == false)
     {
         Serial.println("CLOSED");
         servoList[servoIndex].write(SERVO_CLOSED);
+        digitalWrite(ledListGreen[servoIndex], ledOff);
+        digitalWrite(ledListRed[servoIndex], ledOn);
     }
 }
 
@@ -181,4 +217,3 @@ void setLEDYellow(int index)
     strip.setPixelColor(index,10,10,0);
     strip.show();
 }
-
