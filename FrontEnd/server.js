d9b1dd4c384e8ed891b5209e13a441b966b51cc6
root <root@ip-172-31-6-106.us-west-2.compute.internal>
2017-11-17 21:36:35 +0100

    test

--- FrontEnd/server.js
+++ FrontEnd/server.js
@@ -1,14 +1,54 @@
 var http = require('http'),
     fs = require('fs');
 
+http.createServer(function (req, res) {
+  console.log(req.url);
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(3456);
-});
+    if(req.url.indexOf('.html') != -1){ //req.url has the pathname, check if it conatins '.html'
+
+      fs.readFile(__dirname + '/index.html', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+      });
+
+    }
+
+    if(req.url.indexOf('.js') != -1){ //req.url has the pathname, check if it conatins '.js'
+
+      fs.readFile(__dirname + '/script.js', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'text/javascript'});
+        res.write(data);
+        res.end();
+      });
+
+    }
+
+
+    if(req.url.indexOf('.css') != -1){ //req.url has the pathname, check if it conatins '.css'
+       fs.readFile(__dirname + '/default.css', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.write(data);
+        res.end();
+      });
+
+    }
+
+
+    if(req.url.indexOf('.css') != -1){ //req.url has the pathname, check if it conatins '.css'
+       fs.readFile(__dirname + '/fonts.css', function (err, data) {
+        if (err) console.log(err);
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.write(data);
+        res.end();
+      });
+
+    }
+
+
+
+}).listen(8080);
+console.log('Server running on port 8080');
