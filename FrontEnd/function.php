23eedccd3ab52976ce74ec3eb5681dd43851aef7
Jack Leshem <jsleshem@wustl.edu>
2017-12-05 01:21:16 -0600

    PHP param

---
+++ FrontEnd/function.php
@@ -0,0 +1,13 @@
+<?php
+
+  //  if (isset($_POST['id'])) {
+
+      //  deleteClient11($_POST['id']);
+
+        //function deleteClient11($x) {
+           echo "Hello!";
+        //}
+
+  //  }
+
+?>
